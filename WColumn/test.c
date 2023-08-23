66cb394b4e4aeb9fcc2399b8888e3637cd877e5f
Love Pengy <afrazierboy2001@gmail.com>
2023-08-22 21:40:17 -0400

    Space Logic Fixed Last Line Crashing

--- WColumn/test.c
+++ WColumn/test.c
@@ -6,20 +6,29 @@
  char * stringParse(char *pString){
         //printf("DEBUG1!!%s\n", pString);
         int size = strlen(pString);
+        printf("Something: %d\n", size);
         //printf("size: %d\n", size);
         char *newString = malloc(sizeof(char) * (size + 1));
         int j = 0;
+        int spaceCount = 0;
+        int countValid = 0;
+        
         for(int i = 0; i < size; i++){
             if(!isspace(pString[i])){
                 newString[i-j] = pString[i];
+                spaceCount = 0;
+                countValid++;
                 //printf("Added: %c\n", pString[i]);
                 //printf("NewString: %s\n", newString);
+        }
+        else if((spaceCount == 0) && (countValid > 0)){
+            newString[i-j] = pString[i];
+            spaceCount++;
         }
             else{
                 j++;
             }
      }
-     //printf("DEBUG2!!%s\n", newString);
      newString[size-j] = '\0';
      return(newString);
     }
@@ -36,6 +45,22 @@ int main (void){
     fgets(string, 80, fp);
     string = stringParse(string);
     printf("STRING %d: %s\n", i, string);
-    printf("got to the end\n");
+    i++;
+    fgets(string, 80, fp);
+    string = stringParse(string);
+    printf("STRING %d: %s\n", i, string);
+    i++;
+    fgets(string, 80, fp);
+    string = stringParse(string);
+    printf("STRING %d: %s\n", i, string);
+    i++;
+    fgets(string, 80, fp);
+    printf("inputted string: %s\n", string);
+    string = stringParse(string);
+    printf("STRING %d: %s\n", i, string);
+
+    printf("got to here\n");
+    fflush(stdout);
+    fclose(fp);
     return 0; 
 }
