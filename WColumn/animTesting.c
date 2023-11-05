2eafe6bb89fbfa35973cb30f70ef5c18839a1d4f
Bee <afrazierboy2001@gmail.com>
2023-11-04 22:13:42 -0400

    Start of animations implementation

--- WColumn/animTesting.c
+++ WColumn/animTesting.c
@@ -1,57 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-struct filearr{
+#include <string.h>
+char baseDir[12] = "animations/"; 
+
+struct anim{
         int size; 
-        char ** filePaths;
+        char ** frames; 
+
+
+}; 
+typedef struct anim * animation; 
+
+
+struct animationManager{
+        int numAnimations; 
+        animation * anims;
+
 };
-typedef struct filearr * files; 
+typedef struct animationManager * manager; 
 
-void playAnimation(FILE ** fp){
+void playRandAnimation(manager manage){
+        srand(1); 
+        FILE * holdFp;
+        char * hold; 
+        int picker = (rand() % manage->numAnimations);
+        for(int i = 0; i < manage->anims[picker]->size; i++){ 
+                holdFp = fopen(manage->anims[picker]->frames[i], "r");
+                fseek(holdFp, 0, SEEK_END); 
+                int size = ftell(holdFp); 
+                rewind(holdFp); 
+                hold = malloc(size +1); 
+                fread(hold, size, 1, holdFp); 
+                printf("%s", hold); 
+        }
+}
 
-        //take in an array of file pointers and play through them one by one printing the value and then pausing in between each one
+void append(animation appendee, char* newPath){
+        appendee->frames[appendee->size] = newPath; 
+        appendee->size++;
+}
+
+
+//filenames will come from a file within the folder for the animation
+//take basedir and add the next file name onto it
+char * animationCreator(char * dir, char * fileName){
+        char * hold = malloc(sizeof(char) * (strlen(dir) + strlen(fileName))); 
+        strcpy(hold, dir);
+        strcat(hold, fileName);
+        return(hold);
+}
+
+animation initAnimation(void){
+       animation new = malloc(sizeof(struct anim)); 
+       new->size = 0; 
+       for(int i = 0; i < 10; i++){
+                new->frames[i] = malloc(sizeof(char) * 50);
+        }
+       return(new);
+}
+
+manager initManager(void){
+        manager new = malloc(sizeof(struct animationManager)); 
+        new->numAnimations = 0; 
+        new->anims = malloc(sizeof(struct anim) * 10);
+        return(new);
+}
+
+void animationSetup(manager boss){
+        char * filePath = malloc(sizeof(char) * 24);
+        strcpy(filePath,  baseDir);
+        strcat(filePath, "manage.txt");
+        FILE * fptr = fopen(filePath, "r"); 
+        char * hold = malloc(sizeof(char) * 100);
+        animation currAnim = initAnimation();
+        int numFrames = 0;
+        while(!feof(fptr)){
+                fgets(hold, 99, fptr); 
+                if(hold[0] == '~'){
+                        boss->anims[boss->numAnimations] = currAnim;
+                        boss->numAnimations++;
+                        currAnim = initAnimation();
+                        continue;
+                }
+                hold = animationCreator(baseDir, hold);
+                append(currAnim, hold);
+        }
+                boss->anims[boss->numAnimations] = currAnim; 
+                boss->numAnimations++; 
+
 
 }
 
-void append(files filesStruct, char* newPath){
-        filesStruct->filePaths[filesStruct->size] = newPath; 
-}
 
-void animationCreator(char * dir, char * fileName){
-        
 
-}
-*/
 int main (void){
-
-        char test[100] = "animations/success1/frame1";
-        FILE * testFp = fopen(test, "r");
-        fseek(testFp, 0, SEEK_END); 
-        int size = ftell(testFp);
-        rewind(testFp); 
-
-        char * hold = malloc(size + 1); 
-        fread(hold, size, 1, testFp); 
-        printf("%s", hold); 
-        
+//within this file thre is a ~ and then the folder in which the animation resides
+//then the frame names
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return(0);
 }
